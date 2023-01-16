3ce5406ec1a89bbb07ad20c274a8cc7eb5c05d45
janardhanrp <janardhanrp@maveric-systems.com>
2023-01-16 22:11:18 +0530

    second commit

---
+++ src/main/java/com/maveric/employee/dto/EmployeeDto.java
@@ -0,0 +1,24 @@
+package com.maveric.employee.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class EmployeeDto {
+        @Id
+        private Integer employeeId;
+        private String firstName;
+        private String secondName;
+        private String lastName;
+        private String address;
+        private String phoneNumber;
+        private String email;
+
+
+    }
